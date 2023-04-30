37b98d0bfc2a289bb2d894598f5dd61235118bd7
ANKIT VERMA <vankit10@gmail.com>
2023-04-30 12:25:06 +0530

    final

--- src/Homepage/Homepage.jsx
+++ src/Homepage/Homepage.jsx
@@ -5,40 +5,41 @@ import './Homepage.css'
 const Homepage = () => {
     const questions = [
 		{
-			questionText: 'What is the capital of France?',
-			answerOptions: [
-				{ answerText: 'New York', isCorrect: false },
-				{ answerText: 'London', isCorrect: false },
-				{ answerText: 'Paris', isCorrect: true },
-				{ answerText: 'Dublin', isCorrect: false },
-			],
+		  questionText: "What is the capital of Chhattisgarh?",
+		  answerOptions: [
+			{ answerText: "Ludiyana", isCorrect: false },
+			{ answerText: "Itanagar", isCorrect: false },
+			{ answerText: "Raipur", isCorrect: true },
+			{ answerText: "Patna", isCorrect: false },
+		  ],
 		},
 		{
-			questionText: 'Who is CEO of Tesla?',
-			answerOptions: [
-				{ answerText: 'Jeff Bezos', isCorrect: false },
-				{ answerText: 'Elon Musk', isCorrect: true },
-				{ answerText: 'Bill Gates', isCorrect: false },
-				{ answerText: 'Tony Stark', isCorrect: false },
-			],
+		  questionText: "Who is owner of facebook?",
+		  answerOptions: [
+			{ answerText: "Jeff Bezos", isCorrect: false },
+			{ answerText: "Mark zuckerberg", isCorrect: true },
+			{ answerText: "Bill Gates", isCorrect: false },
+			{ answerText: "Tony Stark", isCorrect: false },
+		  ],
 		},
 		{
-			questionText: 'The iPhone was created by which company?',
-			answerOptions: [
-				{ answerText: 'Apple', isCorrect: true },
-				{ answerText: 'Intel', isCorrect: false },
-				{ answerText: 'Amazon', isCorrect: false },
-				{ answerText: 'Microsoft', isCorrect: false },
-			],
+		  questionText: "when we made our first independent day?",
+		  answerOptions: [
+			{ answerText: "15August1947", isCorrect: true },
+			{ answerText: "1jan1950", isCorrect: false },
+			{ answerText: "15August1949", isCorrect: false },
+			{ answerText: "15August1946", isCorrect: false },
+		  ],
 		},
 		{
-			questionText: 'How many Harry Potter books are there?',
-			answerOptions: [
-				{ answerText: '1', isCorrect: false },
-				{ answerText: '4', isCorrect: false },
-				{ answerText: '6', isCorrect: false },
-				{ answerText: '7', isCorrect: true },
-			],
+		  questionText: "How many Harry Potter books are there?",
+		  answerOptions: [
+			{ answerText: "1", isCorrect: false },
+			{ answerText: "4", isCorrect: false },
+			{ answerText: "6", isCorrect: false },
+			{ answerText: "7", isCorrect: true },
+		  ],
+	 
 		},
 	];
 
